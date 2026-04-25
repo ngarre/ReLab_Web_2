b48be01dd77121ba92b214782c9f7f2f93fdc883
ngarre <a28710@svalero.com>
2026-04-25 16:52:27 +0200

    fix: excluir tests e2e de vitest

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    include: ['src/**/*.{test,spec}.ts', 'src/**/*.{test,spec}.tsx'],
+    exclude: ['e2e/**', 'node_modules/**'],
   },
 })
